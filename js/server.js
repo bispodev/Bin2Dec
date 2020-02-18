5716d4f94fd3ca27aa465ed4afb0bedc56f889b1
João Gustavo <joao.bispo@uvvnet.com.br>
2020-02-18 09:14:42 -0300

    Update
    Alteração nas verificação do Js

--- js/server.js
+++ js/server.js
@@ -1,11 +1,38 @@
+var decimal = document.getElementsByTagName("input")[1]
+
 function binToDec() {
     var dec = 0
     var bin = document.getElementsByTagName("input")[0].value
-    var decimal = document.getElementsByTagName("input")[1]
 
-    for (var index = bin.length - 1; index >= 0; index--) {
-        dec += parseInt(bin[index]) * Math.pow(2, bin.length - 1 - index)
+    if (bin.length > 8) {
+        alert("Insira apenas 8 digitos")
+        console.log("Erro, muitos digitos")
+        return false
     }
-    console.log(dec)
-    decimal.value = dec
+
+    if (verify(bin.toString())) {
+        for (var index = bin.length - 1; index >= 0; index--) {
+            dec += parseInt(bin[index]) * Math.pow(2, bin.length - 1 - index)
+        }
+        decimal.value = dec
+    } else {
+        return false
+    }
+}
+
+function verify(binare) {
+    var bin_split = binare.split('');
+
+    for (var index = bin_split.length - 1; index >= 0; index--) {
+        const split = bin_split[index]
+        if (split == "1" || split == "0") {
+            console.log("Passou no teste" + split)
+        } else {
+            console.log("Não passou no teste: " + split)
+            decimal.value = '';
+            alert("Apenas numeros binarios")
+            return false
+        }
+    }
+    return true
 }
